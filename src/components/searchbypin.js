172dec2e6612c272304bc508ae0787d4cd5d4879
Harsh Agrawal <9584292259.harsh@gmail.com>
2021-05-19 16:26:40 +0530

    searchbypin functionality inhanced

--- src/components/searchbypin.js
+++ src/components/searchbypin.js
@@ -1,7 +1,7 @@
 
 import React,{useEffect,useState} from 'react'
 import axios from 'axios'
-
+import {Table} from 'react-bootstrap'
 function Searchbypin(props) {
 
     const[pin,setPin]=useState('')
@@ -11,16 +11,16 @@ function Searchbypin(props) {
     
     useEffect(()=>{
       if(pin.length===6)
-       { axios.get(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pin}&date=${date}`)
+       { axios.get(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pin}&date=${date}`)
         .then(resp=>{
             console.log(resp.data)
             
-            setPindata(resp.data.sessions)
+            setPindata(resp.data.centers)
           })
          .catch(error=> console.log(error))
      } },[pin] )
          
-         
+    
          const showempty=()=>{return (<table >
             <tr>
               <th>Center</th>
@@ -33,22 +33,32 @@ function Searchbypin(props) {
                 <td>NA</td>
                 <td>NA</td>
              </tr></table>)}
-    return (
-        <div>
-            <input type='text' value={pin} onChange={e=>setPin(e.target.value)}></input>
-        
-           
-            
-             
-                  Center
           
-                  Age+
-                  Availability
+    return (
+        
+        
+        <div>
+               
+            <input type='text' value={pin} onChange={e=>setPin(e.target.value)}></input>
+        <br/>
+        {pin.length===6? <h3 style={{color:"grey"}}> Below are details of available slots at {pin} within 7 days</h3>:null}
+    
+            <br/>
                 
-          {pindata?pindata.map(pindatas =>{
+          {pin.length===6?pindata.map(pindatas =>{
                return (
-               <div key={pindatas.center_id}> {pindatas.name } {pindatas.min_age_limit}
-                {pindatas.available_capacity}</div>)}
+                   
+               <div key={pindatas.center_id}><b style={{}}>{pindatas.name }<t/>{pindatas.fee_type==="Free"? 
+               <b style={{color:"blue"}}>{pindatas.fee_type}</b>:<b style={{color:"red"}}>{pindatas.fee_type}</b>}</b>
+               
+                {pindatas.sessions.map(pinsession=>{
+                    return(
+                     <div key={pinsession.session_id}> <b>{ pinsession.date}</b> {" "+pinsession.vaccine+" available doses "}{pinsession.available_capacity===0?<b style={{background:"lightyellow",color:"red",borderRadius:"10%",border:"10%"}}>
+                          Booked</b>:<b style={{background:"lightyellow",color:"green",borderRadius:"10%",border:"10%"}}>{pinsession.available_capacity}</b>}<b>{"   age-limit: "}</b><b style={{color:"brown"}}>{pinsession.min_age_limit}</b>{" (dose1= "+pinsession.available_capacity_dose1+" dose2= "+pinsession.available_capacity_dose2+")"} <br/> </div>
+                    )
+                  
+                })}
+               <br/>  <br/></div>)}
            ):null}
           
          
